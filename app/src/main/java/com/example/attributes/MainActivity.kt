a365926c3272a5e1134d53340af99fa8fdcd5c4a
workspace <sadovikov.ne@kemerovorea.ru>
2021-09-20 15:46:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/attributes/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.attributes
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Size
+import android.util.TypedValue
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val blackText: Button = findViewById(R.id.blacktext)
+        val redText: Button = findViewById(R.id.redtext)
+        val changeSize: Button = findViewById(R.id.spSize8)
+        val changeSize1: Button = findViewById(R.id.spSize24)
+        val changeColor: Button = findViewById(R.id.whitecolor)
+        val changeColor1: Button = findViewById(R.id.yellowcolor)
+        val edittext: EditText = findViewById(R.id.edittext)
+
+        blackText.setOnClickListener {
+
+        }
+        changeSize.setOnClickListener {
+            edittext.setTextSize(8f)
+        }
+        changeSize1.setOnClickListener {
+            edittext.setTextSize(24f)
+        }
+        blackText.setOnClickListener {
+            edittext.setTextColor(Color.BLACK)
+        }
+        redText.setOnClickListener {
+            edittext.setTextColor(Color.RED)
+        }
+        changeColor.setOnClickListener {
+            edittext.setBackgroundColor(Color.WHITE)
+        }
+        changeColor1.setOnClickListener {
+            edittext.setBackgroundColor(Color.YELLOW)
+        }
+    }
+}
